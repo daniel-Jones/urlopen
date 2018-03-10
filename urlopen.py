2aa4329286f4fb1be38558ee487781b3934efc41
daniel-Jones <daniel@danieljon.es>
2018-03-10 20:04:09 +1030

    added GPL licence information

--- urlopen.py
+++ urlopen.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python2.7
+
+# Copyright Daniel Jones 2018
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys, os;
 from urlparse import urlparse;
 def run(process, arg):
@@ -10,7 +26,7 @@ def run(process, arg):
 
 def main(args):
     images = ["jpg", "png"]; imageapp = ["feh"];
-    videos = ["gif", "gifv", "webm", "mp4"]; videoapp = ["mpv", "--loop"];
+    videos = ["gif", "gifv", "webm", "mp4"]; videoapp = ["mpv", "--ytdl-format=22", "--loop"]; #--yt-dlformat=22
     videourls = ["youtube.com", "youtu.be", "streamable.com"];
     pdf = ["pdf"]; pdfapp = ["mupdf"];
     defaultapp = ["/home/daniel_j/compiled/waterfox/waterfox"];
